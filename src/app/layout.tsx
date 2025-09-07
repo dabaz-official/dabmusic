c2ad62726521aff4222213ebfbdeb3ac84eb0a23
DabAZ <dabaz@dabaz.me>
2025-09-07 12:42:11 +0800

    Update font to Geist Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
 
 import './globals.css';
 import Header from '@/components/layout/Header';
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
+        className={`${GeistSans.className} antialiased`}
       >
         <Header />
         {children}
