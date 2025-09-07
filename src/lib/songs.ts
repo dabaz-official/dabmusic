34c52f47518af42c867736bf83b061ae9ce2952e
DabAZ <dabaz@dabaz.me>
2025-09-07 11:01:09 +0800

    Add songs and album covers

---
+++ src/lib/songs.ts
@@ -0,0 +1,23 @@
+export interface Song {
+  title: string;
+  src: string;
+  cover: string;
+}
+
+export const songs: Song[] = [
+  {
+    title: '薄荷水',
+    src: '/audio/bo-he-shui.mp3',
+    cover: '/audio/bo-he-shui.webp'
+  },
+  {
+    title: '可惜你不在',
+    src: '/audio/ke-xi-ni-bu-zai.mp3',
+    cover: '/audio/na-xiao-zi-zhen-shuai.webp'
+  },
+  {
+    title: '下坡',
+    src: '/audio/xia-po.mp3',
+    cover: '/audio/na-xiao-zi-zhen-shuai.webp'
+  },
+];
