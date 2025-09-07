4441c8c98d202cb16fe0215ee2099ff99c9667e0
DabAZ <dabaz@dabaz.me>
2025-09-07 14:17:08 +0800

    Update songs.ts

--- src/lib/songs.ts
+++ src/lib/songs.ts
@@ -2,22 +2,30 @@ export interface Song {
   title: string;
   src: string;
   cover: string;
+  artist: string;
+  isExplicit: boolean;
 }
 
 export const songs: Song[] = [
   {
     title: '薄荷水',
     src: '/audio/bo-he-shui.mp3',
-    cover: '/audio/bo-he-shui.webp'
+    cover: '/images/bo-he-shui.webp',
+    artist: 'SASIOVERLXRD',
+    isExplicit: true,
   },
   {
     title: '可惜你不在',
     src: '/audio/ke-xi-ni-bu-zai.mp3',
-    cover: '/audio/na-xiao-zi-zhen-shuai.webp'
+    cover: '/images/na-xiao-zi-zhen-shuai.webp',
+    artist: 'SASIOVERLXRD',
+    isExplicit: true,
   },
   {
     title: '下坡',
     src: '/audio/xia-po.mp3',
-    cover: '/audio/na-xiao-zi-zhen-shuai.webp'
+    cover: '/images/na-xiao-zi-zhen-shuai.webp',
+    artist: 'SASIOVERLXRD',
+    isExplicit: true,
   },
 ];
